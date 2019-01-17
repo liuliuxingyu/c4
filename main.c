3cc71111558eebe4f97843ed2a040b93ca10a501
邵楠 <1985890648@qq.com>
2019-01-17 08:38:54 +0800

    添加敏感字符

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+char arr[1024];
+int index=0;
 int main(){
-    printf("hello");
+    printf("minganzifuguanlixitong");
+    while(1){
+        printf("1---tianjiaminganzifu\n");
+        printf("2---shanchuminganzifu(zuihouyige)\n");
+        printf("3---chakanminganzifu\n");
+        printf("4---tihuanminganzifu\n");
+        printf("5---htuichu\n");
+        printf("qingshuruxiangyingdegongnengbianhao:\n");
+        int code;
+        scanf("%d",&code);
+        if(code==1){
+            printf("qingshuruyaotianjiademinganzifu:\n");
+            char m;
+            scanf("%c",&m);
+            scanf("%c",&m);
+            arr[index]=m;
+            index++;
+            printf("tianjiachenggong,dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+
+        }
+         if(code==2){
+            
+        }
+         if(code==3){
+            
+        }
+         if(code==4){
+            
+        }
+         if(code==5){
+            
+        }
+    }
     return 0;
 
 }                                                                                                       
