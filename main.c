f0b2010ff73d6f8d8adf6c5fde22d49f177dfd0c
邵楠 <1985890648@qq.com>
2019-01-17 08:47:12 +0800

    查看敏感字符

--- main.c
+++ main.c
@@ -36,6 +36,14 @@ int main(){
             
         }
          if(code==3){
+             printf("suoyouminganzifuruxia\n");
+             for(int i=0;i<index;i++){
+                 printf("di%dgeminganzifuwei:%c\n",i+1,arr[i]);
+             }
+             printf("chakanchenggong,dianjihuichejixu\n");
+             char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
             
         }
          if(code==4){
